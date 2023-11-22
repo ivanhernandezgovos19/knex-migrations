22354abe0cab4ce026356c51fe221efe846c7fe2
Ivan Hernandez <ivan.hernandez@globallogic.com>
2023-11-22 15:02:38 +0000

    Update dim_jurisdictions & dim_locations tables

---
+++ knex/migrations/20231122145943_update_dim_locations.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table('dim_locations', function(table) {
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table('dim_locations', function(table) {
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
+  });
+}
+
